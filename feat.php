e6b7772b28d3f00febca504d7af383aff227c3a7
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2020-08-12 20:21:20 +0200

    Create feat.php

---
+++ feat.php
@@ -0,0 +1,31 @@
+<?php  
+
+?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <title>Re:Ply.org</title>
+ <link href="https://fonts.googleapis.com/css?family=Quicksand|Varela+Round&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+ 
+ <body>
+ 
+ <header class="Landingheader">
+ <a href="index.php">
+ <img src="logo.svg" class="Logo" style="user-select:none;"> 
+ </a>
+ <nav>
+ <ul>
+ <li><a href="feat.php">Features</a></li>
+  <li><a href="feed.php">Feedback</a></li>
+ <li><a href="about.php">About Us</a></li>
+ <li class="signIn"><a href="logIn.php" id="signInside">Sign Up</a></li>
+ </ul>
+ </nav>
+ </header>
+ <h1> FEAT . PHP <h1>
+ </body>
+ </html>
