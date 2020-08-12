2c41a64e6bf4599fefa879f2b8ae9794f73c9105
[CD] Raiko <64981218+CodeNameRaiko@users.noreply.github.com>
2020-08-12 19:57:24 +0200

    Create index.php
    The landing page

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php  
+
+?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <title>Re:Ply.org</title>
+ <link href="https://fonts.googleapis.com/css?family=Quicksand|Varela+Round&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+ 
+ <body class="container">
+
+ <header class="Landingheader">
+ <a href="index.php">
+ <img src="logo.svg" class="Logo" style="user-select:none;"> 
+ </a>
+ <nav>
+ <ul>
+  <li><a href="about.php">About Us</a></li>
+ <li><a href="feat.php">Features</a></li>
+  <li><a href="feed.php">Feedback</a></li>
+ <li class="signIn"><a href="login.php" id="signInside">Sign Up</a></li>
+ </ul>
+ </nav>
+ </header>
+
+ <div class="mainsvg">
+<div class="slogan">
+ <p class="catchLine">Need sombody to talk to?</p>
+ <p class="punchLine">You can <span class="rely">rely</span> on us!</p>
+</div>
+
+ <p class="mobileAnnouncement" position:absolute;>In both App Store & Google Play</p> 
+  <div class="align" style="display:inline-flex; position:absolute; left:900px;">
+  <img src="appstore.svg" class="AppStore">
+  <img src="googleplay.svg" class="GooglePlay">
+</div>
+
+<div class="alignphones" style="position:relative; left:1560px; bottom:267px;">
+<img src="Phones.png" id="phoneSVG" style="user-select:none;">
+</div>
+</div>
+
+<div class="helpAlign" style="position:absolute; left:96px;">
+<h1 class="needHelp">Need any help?</h1>
+<p class="pHelp">
+ <p class="first ">If there are some question you got then we got you on that!</p>
+<br>
+<span class="last">Here are some of the most frequently asked questions</span></p>
+<button class="learnMore">Learn More</button>
+
+</div>
+ 
+ </body>
+ </html>
+
+ <!-- left:1700px; bottom:267px; -->
